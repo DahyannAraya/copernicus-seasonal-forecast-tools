4fcfe93964ccc97f0a5bd1798b682831bd2e706f
DahyannAraya <inimaginable11@gmail.com>
2025-05-08 22:29:06 +0200

    Update custom.css

--- docs/make.bat
+++ docs/make.bat
@@ -1,35 +1,24 @@
 @ECHO OFF
 
-pushd %~dp0
-
 REM Command file for Sphinx documentation
 
-if "%SPHINXBUILD%" == "" (
-	set SPHINXBUILD=sphinx-build
-)
-set SOURCEDIR=source
-set BUILDDIR=build
+SET SPHINXBUILD=sphinx-build
+SET SOURCEDIR=source
+SET BUILDDIR=build
 
-%SPHINXBUILD% >NUL 2>NUL
-if errorlevel 9009 (
-	echo.
-	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
-	echo.installed, then set the SPHINXBUILD environment variable to point
-	echo.to the full path of the 'sphinx-build' executable. Alternatively you
-	echo.may add the Sphinx directory to PATH.
-	echo.
-	echo.If you don't have Sphinx installed, grab it from
-	echo.https://www.sphinx-doc.org/
-	exit /b 1
+IF "%SPHINXBUILD%" == "" (
+    ECHO.
+    ECHO The 'sphinx-build' command was not found. Make sure you have Sphinx installed,
+    ECHO then set the SPHINXBUILD environment variable to point to the full
+    ECHO path of the 'sphinx-build' executable. Alternatively, add it to your PATH.
+    ECHO.
+    EXIT /B 1
 )
 
-if "%1" == "" goto help
+%SPHINXBUILD% -b html %SOURCEDIR% %BUILDDIR%\html
+IF ERRORLEVEL 1 EXIT /B 1
 
-%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
-goto end
+ECHO.
+ECHO Build finished. The HTML pages are in %BUILDDIR%\html.
 
-:help
-%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 
-:end
-popd
